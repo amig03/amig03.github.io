3a2c0c99bfe19046f0a7c7e1955094fc77364965
amig03 <32470081+amig03@users.noreply.github.com>
2018-04-17 09:11:48 +0300

    Add files via upload

---
+++ Web Development/js/gallery.js
@@ -0,0 +1,85 @@
+var modal = document.getElementById("modal_background");
+var body = document.getElementsByTagName("body")[0];
+var images = document.querySelectorAll(".gallery_container img");
+var right_control = document.getElementById("right_control");
+var left_control = document.getElementById("left_control");
+var controls = [left_control, right_control, document];
+
+var paging_events = ["click", "touchstart", "keypress"];
+
+// Текущее изображение
+
+var cur_image = {
+	index: undefined,
+	item: undefined,
+	scale: 1
+};
+
+// Добавляем модалку для просмотра изображения
+
+Array.prototype.forEach.call(images, function (item, i) {
+	item.addEventListener("click", function () {
+		item.classList.add("modal_image");
+		modal.style.display = "block";
+		body.style.overflow = "hidden";
+		cur_image.item = item;
+		cur_image.index = i;
+		cur_image.scale = 1;
+	});
+});
+
+// Добавляем обработчик для выхода из режима просмотра
+
+modal.addEventListener("click", function () {
+	if (cur_image.item == undefined) return;
+
+	cur_image.item.classList.remove("modal_image");
+	modal.style.display = "none";
+	body.style.overflow = "auto";
+
+	cur_image.item.style.transform = "";
+	cur_image.item = undefined;
+});
+
+// Перелистывание фотографий
+
+addEventHandler(controls, "click touchstart keydown", function (e) {
+	if (cur_image.item == undefined) return;
+	console.log(e);
+	e.stopPropagation();
+
+	var index = undefined;
+
+	if (this == left_control || e.keyCode == 37) {
+		index = cur_image.index - 1;
+		if (images[index] == undefined) index = images.length - 1;
+	} else if (this == right_control || e.keyCode == 39) {
+		index = cur_image.index + 1;
+		if (images[index] == undefined) index = 0;
+	}
+
+	if (index == undefined) return;
+
+	cur_image.scale = 1;
+	cur_image.item.style.transform = "";
+
+	images[index].classList.add("modal_image");
+	cur_image.item.classList.remove("modal_image");
+
+	cur_image.item = images[index];
+	cur_image.index = index;
+});
+
+// Изменение масштаба фото
+
+addEventHandler(document, "wheel", function (e) {
+	if (cur_image.item == undefined) return;
+
+	if (e.deltaY > 0 && cur_image.scale >= 0.3) {
+		cur_image.scale -= 0.1;
+	} else if (e.deltaY < 0) {
+		cur_image.scale += 0.1;
+	}
+
+	cur_image.item.style.transform = "translate(-50%, -50%) scale(" + cur_image.scale + ")";
+});
